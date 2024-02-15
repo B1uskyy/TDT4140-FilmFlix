2835d8e2953a22cd6ad0684cbecddc62608aff92
Mats <mats.kvanvik@hotmail.com>
2024-02-15 15:18:05 +0100

    feat #3: add styling to login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./../styles/login.css";
 
 function Login(props) {
 	const [email, setEmail] = useState("");
@@ -9,15 +10,44 @@ function Login(props) {
 
 	const navigate = useNavigate();
 
+	const onButtonClick = () => {
+		// You'll update this function later...
+	};
+
 	return (
-		<div className={"inputContainer"}>
-			<input
-				value={email}
-				placeholder="Enter your email here"
-				onChange={(ev) => setEmail(ev.target.value)}
-				className={"inputBox"}
-			/>
-			<label className="errorLabel">{emailError}</label>
+		<div className={"mainContainer"}>
+			<div className={"titleContainer"}>
+				<div>Login</div>
+			</div>
+			<br />
+			<div className={"inputContainer"}>
+				<input
+					value={email}
+					placeholder="Enter your email here"
+					onChange={(ev) => setEmail(ev.target.value)}
+					className={"inputBox"}
+				/>
+				<label className="errorLabel">{emailError}</label>
+			</div>
+			<br />
+			<div className={"inputContainer"}>
+				<input
+					value={password}
+					placeholder="Enter your password here"
+					onChange={(ev) => setPassword(ev.target.value)}
+					className={"inputBox"}
+				/>
+				<label className="errorLabel">{passwordError}</label>
+			</div>
+			<br />
+			<div className={"inputContainer"}>
+				<input
+					className={"inputButton"}
+					type="button"
+					onClick={onButtonClick}
+					value={"Log in"}
+				/>
+			</div>
 		</div>
 	);
 }
