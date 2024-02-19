58056e12655501ef22252f17bf68ae942a058810
Mats <mats.kvanvik@hotmail.com>
2024-02-19 09:14:11 +0100

    style #3: added styling to match figma design

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./../styles/login.css";
+import posters from "./../img/posters.jpg";
 
 function Login(props) {
 	const [email, setEmail] = useState("");
@@ -8,6 +9,7 @@ function Login(props) {
 	const [emailError, setEmailError] = useState("");
 	const [passwordError, setPasswordError] = useState("");
 
+	// eslint-disable-next-line
 	const navigate = useNavigate();
 
 	const onButtonClick = () => {
@@ -21,7 +23,9 @@ function Login(props) {
 			return;
 		}
 
+		// eslint-disable-next-line
 		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+			// REGEX for å oppgi en valid epost
 			setEmailError("Please enter a valid email");
 			return;
 		}
@@ -31,47 +35,50 @@ function Login(props) {
 			return;
 		}
 
-		if (password.length < 7) {
-			setPasswordError("The password must be 8 characters or longer");
-			return;
-		}
-
 		// Authentication calls will be made here...
 	};
 
 	return (
 		<div className={"mainContainer"}>
-			<div className={"titleContainer"}>
-				<div>Login</div>
+			<div className="leftSide">
+				<img src={posters} alt="poster" className="posterImage" />
 			</div>
-			<br />
-			<div className={"inputContainer"}>
-				<input
-					value={email}
-					placeholder="Enter your email here"
-					onChange={(ev) => setEmail(ev.target.value)}
-					className={"inputBox"}
-				/>
-				<label className="errorLabel">{emailError}</label>
-			</div>
-			<br />
-			<div className={"inputContainer"}>
-				<input
-					value={password}
-					placeholder="Enter your password here"
-					onChange={(ev) => setPassword(ev.target.value)}
-					className={"inputBox"}
-				/>
-				<label className="errorLabel">{passwordError}</label>
-			</div>
-			<br />
-			<div className={"inputContainer"}>
-				<input
-					className={"inputButton"}
-					type="button"
-					onClick={onButtonClick}
-					value={"Log in"}
-				/>
+			<div className="rightSide">
+				<h1 className="filmFlixTitle">FilmFlix</h1>
+				<div className={"titleContainer"}>
+					<div>Login</div>
+				</div>
+				<br />
+				<div className={"inputContainer"}>
+					<p className="inputTitle">Username</p>
+					<input
+						value={email}
+						placeholder="Enter your email here"
+						onChange={(ev) => setEmail(ev.target.value)}
+						className={"inputBox"}
+					/>
+					<label className="errorLabel">{emailError}</label>
+				</div>
+				<br />
+				<div className={"inputContainer"}>
+					<p className="inputTitle">Password</p>
+					<input
+						value={password}
+						placeholder="Enter your password here"
+						onChange={(ev) => setPassword(ev.target.value)}
+						className={"inputBox"}
+					/>
+					<label className="errorLabel">{passwordError}</label>
+				</div>
+				<br />
+				<div className={"inputContainer"}>
+					<input
+						className={"inputButton"}
+						type="button"
+						onClick={onButtonClick}
+						value={"Log in"}
+					/>
+				</div>
 			</div>
 		</div>
 	);
