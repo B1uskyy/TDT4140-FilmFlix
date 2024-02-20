b02f37bc708b7480de2daa3c6b1609e2aaf6a873
Adrian Lillevoll <adriasl@stud.ntnu.no>
2024-02-20 14:13:54 +0100

    Oppdaterte App.js med å legge inn sessionProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import "./App.css"; // eslint-disable-next-line
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import { SessionProvider } from "./SessionProvider";
 
 function App() {
 	return (
-		<div>
-			<Router>
-				<Routes>
-					<Route path="/Login" exact={true} element={<Login />} />
-					<Route path="/Homepage" />
-				</Routes>
-			</Router>
-		</div>
+    <SessionProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/Login" exact={true} element={<Login />} />
+            <Route path="/Homepage" />
+				  </Routes>
+			  </Router>
+		  </div>
+    </SessionProvider>
 	);
 }
 
