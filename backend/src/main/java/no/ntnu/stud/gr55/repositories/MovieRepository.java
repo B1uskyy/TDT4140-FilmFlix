1095964f14b3e7cbc3f19ac924588dde79711f6e
bartoszrs <bartosrs@stud.ntnu.no>
2024-02-21 13:57:13 +0100

    feature: backend added filtering option

--- backend/src/main/java/no/ntnu/stud/gr55/repositories/MovieRepository.java
+++ backend/src/main/java/no/ntnu/stud/gr55/repositories/MovieRepository.java
@@ -2,9 +2,12 @@ package no.ntnu.stud.gr55.repositories;
 
 import no.ntnu.stud.gr55.entities.Movie;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +18,12 @@ public interface MovieRepository extends ListCrudRepository<Movie, String>, Pagi
     @Cacheable("genres")
     @Query("SELECT DISTINCT elements(m.genres) FROM Movie m")
     public List<String> getDistinctGenres();
+
+    @Query("SELECT m FROM Movie m WHERE (:year IS NULL OR m.year = :year) AND " +
+            "(:director IS NULL OR EXISTS (SELECT 1 FROM m.directors d WHERE d.id = :director)) AND " +
+            "(:genre IS NULL OR :genre MEMBER OF m.genres)")
+    Page<Movie> findMoviesFiltered(@Param("year") Integer year,
+             @Param("director") String director,
+              @Param("genre") String genre,
+               Pageable page);
 }
