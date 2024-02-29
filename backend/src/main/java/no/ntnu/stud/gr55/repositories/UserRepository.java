2c23bfafc62dba65363bb73fd686b39da05ebce2
Mats <mats.kvanvik@hotmail.com>
2024-02-29 11:49:05 +0100

    feat #9: fixed queries and user entitiy

--- backend/src/main/java/no/ntnu/stud/gr55/repositories/UserRepository.java
+++ backend/src/main/java/no/ntnu/stud/gr55/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package no.ntnu.stud.gr55.repositories;
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,15 +11,19 @@ import no.ntnu.stud.gr55.entities.User;
 
 
 
-public interface UserRepository extends ListCrudRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, Long>{
     
 
 
-@Query("SELECT u FROM User u WHERE ((:password = u.password) AND (:username = u.username) )")
-User findUser(@Param("username") String username, @Param("password") String password);
+// @Query("SELECT u FROM User u WHERE ((:password = u.password) AND (:username = u.username) )")
+// User findUser(@Param("username") String username, @Param("password") String password);
 
-@Query("INSERT INTO User VALUES (:username, :password)")
-void registerUser(@Param("username") String username, @Param("password") String password); 
+// @Query("INSERT INTO User VALUES (:username, :password)")
+// void registerUser(@Param("username") String username, @Param("password") String password); 
+
+
+List<User> findByUserNameAndPassword(String username, String password); 
+User save(User user); 
 
     
 }
