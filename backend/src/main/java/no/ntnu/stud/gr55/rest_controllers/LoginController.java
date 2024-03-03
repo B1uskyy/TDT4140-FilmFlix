ccbc141231d32e5d574b59e7b66e97afd8428f66
Mats <mats.kvanvik@hotmail.com>
2024-03-03 15:40:33 +0100

    feat #9: started frontend implementation

--- backend/src/main/java/no/ntnu/stud/gr55/rest_controllers/LoginController.java
+++ backend/src/main/java/no/ntnu/stud/gr55/rest_controllers/LoginController.java
@@ -7,45 +7,69 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import no.ntnu.stud.gr55.entities.User;
+import no.ntnu.stud.gr55.repositories.UserRepository;
 
+@RestController
+@RequestMapping("/api")
 public class LoginController {
 
-    @getMapping
+      @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> loginRequest) {
-        String email = loginRequest.get("email");
-        String password = loginRequest.get("password");
+
+    @GetMapping("/users/all")
+    public List<User> getAllUsers() {
+        return userRepository.getUsers(); 
+    }
+
+  
+
+
+    @PostMapping("/users")
+    public User register(@RequestBody User user) {
+        // ResponseEntity<Map<String, String>>
+        // String email = loginRequest.get("email");
+        // String password = loginRequest.get("password");
+
+
+        return userRepository.save(user); 
+
+
 
     
-        if (isValidCredentials(email, password)) {
-            // Return success response
-            Map<String, String> response = new HashMap<>();
-            response.put("message", "Login successful");
-            return ResponseEntity.ok(response);
-        } else {
-            // Attempt to register the user
-            if (registerUser(email, password)) {
-                // Return success response after registration
-                Map<String, String> response = new HashMap<>();
-                response.put("message", "Registration successful. You can now log in.");
-                return ResponseEntity.ok(response);
-            } else {
-                // Return error response if registration fails
-                Map<String, String> response = new HashMap<>();
-                response.put("message", "Invalid email or password. Registration failed.");
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-            }
-        }
+        // if (isValidCredentials(email, password)) {
+        //     // Return success response
+        //     Map<String, String> response = new HashMap<>();
+        //     response.put("message", "Login successful");
+        //     return ResponseEntity.ok(response);
+        // } else {
+        //     // Attempt to register the user
+        //     if (registerUser(email, password)) {
+        //         // Return success response after registration
+        //         Map<String, String> response = new HashMap<>();
+        //         response.put("message", "Registration successful. You can now log in.");
+        //         return ResponseEntity.ok(response);
+        //     } else {
+        //         // Return error response if registration fails
+        //         Map<String, String> response = new HashMap<>();
+        //         response.put("message", "Invalid email or password. Registration failed.");
+        //         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        //     }
+        // }
     }
 
     private boolean isValidCredentials(String email, String password) {
