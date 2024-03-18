aa6674c6cc943c72973f664c6c77d2923cc92d6b
Mats <mats.kvanvik@hotmail.com>
2024-03-18 08:40:46 +0100

    feat #20: made reviewBox component
    Found bug in login that it does not work as intented anymore, even though the code in the pages has not been changed, suspect it might have something to do with the review beeing added to the user JSON file?

--- frontend/src/components/review/ReviewBox.js
+++ frontend/src/components/review/ReviewBox.js
@@ -1,58 +1,120 @@
-import React, { useState } from "react";
-import "./StarRating.css";
-import axios from "axios";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+// import "./login.css";
+import posters from "../../img/posters.jpg";
+import FilmFlixLogo from "../../img/FilmFlixLogo.svg";
 import { useUser } from "../../helpers/UserContext";
 
+// import Logo from "./../components/Logo";
 
-const StarRating = (props) => {
-    const [rating, setRating] = useState(0);
-    const [hover, setHover] = useState(0);
-    const [comment, setComment] = useState("");
-    const { movieId } = props;
-    const user = useUser();
+function Login(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [successfulLogin, setSuccessfulLogin] = useState("");
+    const { setUser } = useUser();
 
-    const handleSubmit = async () => {
+    // eslint-disable-next-line
+    const navigate = useNavigate();
+
+    const validateCredentials = async () => {
+        setSuccessfulLogin("");
+
         try {
-            const response = await axios.post(`/api/movies/view/${movieId}/review`, {
-                rating,
-                review: comment,
+            const response = await fetch("http://localhost:8080/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password }),
             });
-            if (response.data.status === "ok") {
+            console.log(response);
+
+            if (!response.ok) {
+                setSuccessfulLogin("Wrong username og password!");
+                throw new Error(
+                    "Failed to authenticate user. Please check your credentials.",
+                );
             }
+
+            setUser({ username }); // Update context with the username
+            setSuccessfulLogin("Login successfull");
+            navigate("/");
         } catch (error) {
-            // Handle error (e.g., showing an error message)
+            console.log(`Error: ${error}`);
         }
     };
 
+    const onButtonClick = () => {
+        // Set initial error values to empty
+        setEmailError("");
+        setPasswordError("");
+        setSuccessfulLogin("");
 
+        if ("" === username) {
+            // Check if the user has entered both fields correctly
+            setEmailError("Please enter your username");
+            return;
+        }
+
+        if ("" === password) {
+            setPasswordError("Please enter a password");
+            return;
+        }
+
+        if (!emailError && !passwordError) {
+            validateCredentials();
+        }
+    };
 
     return (
-        <div className="star-rating">
-            {[...Array(5)].map((star, index) => {
-                index += 1;
-                return (
-                    <button
+        <div className={"mainContainer"}>
+            <div className="leftSide">
+                <img src={posters} alt="poster" className="posterImage" />
+            </div>
+            <div className="rightSide">
+                {/* <Logo /> */}
+                <img src={FilmFlixLogo} alt="Logo" className="filmFlixLogo" />
+                <div className={"inputContainer"}>
+                    <p className="inputTitle">Username</p>
+                    <br />
+                    <input
+                        value={username}
+                        placeholder="Enter your username here"
+                        onChange={(ev) => setUsername(ev.target.value)}
+                        className={"inputBox"}
+                    />
+                    <label className="errorLabel">{emailError}</label>
+                </div>
+                <br />
+                <div className={"inputContainer"}>
+                    <p className="inputTitle">Password</p>
+                    <br />
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="Enter your password here"
+                        onChange={(ev) => setPassword(ev.target.value)}
+                        className={"inputBox"}
+                    />
+                    <label className="errorLabel">{passwordError}</label>
+                </div>
+                <br />
+                <div className={"inputContainer"}>
+                    <input
+                        className={"inputButton"}
                         type="button"
-                        key={index}
-                        className={index <= (hover || rating) ? "on" : "off"}
-                        onClick={() => {
-                            setRating(index);
-
-                        }}
-                        onMouseEnter={() => setHover(index)}
-                        onMouseLeave={() => setHover(rating)}
-                    >
-                        <span className="star">&#9733;</span>
-                    </button>
-                );
-            })}
-            <textarea
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                placeholder="Write your review here"
-            />
-            <button id="submit-button" onClick={handleSubmit()}>Submit Review</button>
+                        onClick={onButtonClick}
+                        value={"Enter FilmFlix"}
+                    />
+                    <label className="successfulLogin">{successfulLogin}</label>
+                </div>
+                <a href="/register" className="signupLink">
+                    {" "}
+                    New user? Sign up
+                </a>
+            </div>
         </div>
     );
-};
-export default StarRating;
+}
+
+export default Login;
